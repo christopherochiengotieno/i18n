368fd5ce4d98b6beb0718625a6b1d7fe5b7fe432
OTIENO CHRISTOPHER <christopher.otieno@turnkeyafrica.com>
2023-06-16 08:52:52 +0300

    initial setup

---
+++ i18n-backend/src/main/java/com/example/i18nbackend/I18nBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.i18nbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18nBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I18nBackendApplication.class, args);
+    }
+
+}
